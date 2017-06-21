802c6dee695132e4f381a269dd4cb4774e4070be
tomasmaiorino <tomasmaiorino@gmail.com>
2017-06-21 01:00:04 -0300

    initial parser tests

--- src/test/java/com/tsm/sendemail/parser/ClientParserTest.java
+++ src/test/java/com/tsm/sendemail/parser/ClientParserTest.java
@@ -1,26 +1,40 @@
 package com.tsm.sendemail.parser;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
 
-import java.util.Set;
-
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
-
-import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
+import com.tsm.sendemail.model.Client;
 import com.tsm.sendemail.resources.ClientResource;
 import com.tsm.sendemail.util.ClientTestBuilder;
 
 @FixMethodOrder(MethodSorters.JVM)
 public class ClientParserTest {
 
+	private ClientParser parser = new ClientParser();
+
+	@Test(expected = IllegalArgumentException.class)
+	public void toModel_NullResourceGiven_ShouldThrowException() {
+		// Set up
+		ClientResource resource = null;
+
+		// Do test
+		parser.toModel(resource);
+	}
 	
+	@Test
+	public void toModel_ValidResourceGiven_ShouldCreateClientModel() {
+		// Set up
+		ClientResource resource = ClientTestBuilder.buildResoure();
+
+		// Do test
+		Client result = parser.toModel(resource);
+		
+		// Assertions 
+		assertNotNull(result);
+//		assertThat(result, allOf(hasProperty("", is(resource.getName()))));
+		
+	}
 }
