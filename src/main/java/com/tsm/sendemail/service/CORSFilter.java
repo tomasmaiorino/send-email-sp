c517af9bcc93fbdd487ba29b3135709051da3507
tomasmaiorino <tomasmaiorino@gmail.com>
2017-07-13 12:28:15 -0300

    Update CORSFilter.java

--- src/main/java/com/tsm/sendemail/service/CORSFilter.java
+++ src/main/java/com/tsm/sendemail/service/CORSFilter.java
@@ -23,48 +23,51 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class CORSFilter implements Filter {
 
-	@Autowired
-	private ClientService clientService;
+    @Autowired
+    private ClientService clientService;
 
-	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-			throws IOException, ServletException {
-		HttpServletResponse response = (HttpServletResponse) res;
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+        throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
 
-		String clientToken = getClientToken(req);
-		if (StringUtils.isNoneBlank(clientToken)) {
-			response.setHeader("Access-Control-Allow-Origin", clientToken);
-			response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-			response.setHeader("Access-Control-Max-Age", "3600");
-			response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-			response.setHeader("Access-Control-Expose-Headers", "Location");
-		}
+        String clientHost = getClientHost(request);
+        if (!StringUtils.isBlank(clientHost)) {
+            response.setHeader("Access-Control-Allow-Origin", clientHost);
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            response.setHeader("Access-Control-Expose-Headers", "Location");
+        } else {
+            log.info("None header should be added to the host [{}].", request.getRequestURL().toString());
+        }
 
-		chain.doFilter(req, res);
-	}
+        chain.doFilter(req, res);
+    }
 
-	private String getClientToken(final ServletRequest req) {
-		String host = null;
-		String token = "";
-		try {
-			String request = ((HttpServletRequest) req).getRequestURI();
-			log.debug("parsing request [{}].", request);
+    private String getClientHost(final HttpServletRequest req) {
+        String host = null;
+        String token = "";
+        try {
+            String request = req.getRequestURI();
+            log.debug("parsing request [{}].", request);
 
-			token = request.substring(request.lastIndexOf("/") + 1, request.length());
-			log.info("locking for a client with the token [{}].", token);
+            token = request.substring(request.lastIndexOf("/") + 1, request.length());
+            log.info("locking for a client with the token [{}].", token);
 
-			Client client = clientService.findByToken(token);
-			host = client.getClientHosts().iterator().next().getHost();
+            Client client = clientService.findByToken(token);
+            host = client.getClientHosts().iterator().next().getHost();
 
-		} catch (Exception e) {
-			log.info("Client not found [{}].", token);
-		}
-		return host;
-	}
+        } catch (Exception e) {
+            log.info("Client not found [{}].", token);
+        }
+        return host;
+    }
 
-	public void init(FilterConfig filterConfig) {
-	}
+    public void init(FilterConfig filterConfig) {
+    }
 
-	public void destroy() {
-	}
+    public void destroy() {
+    }
 
 }
