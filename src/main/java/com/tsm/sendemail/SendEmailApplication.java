ab3cd07e1f374c8eb1e9721dfa5a313c4e55b15f
tomasmaiorino <tomasmaiorino@gmail.com>
2018-06-12 22:38:08 -0300

    add eureka configuration

--- src/main/java/com/tsm/sendemail/SendEmailApplication.java
+++ src/main/java/com/tsm/sendemail/SendEmailApplication.java
@@ -2,12 +2,14 @@ package com.tsm.sendemail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableEurekaClient
 public class SendEmailApplication {
 
 	public static void main(String[] args) {
