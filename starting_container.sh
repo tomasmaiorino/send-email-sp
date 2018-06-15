50bb8029807e078045f5001caebd215645a01eda
tomas maiorino <tomasmaiorino@gmail.com>
2018-06-15 14:41:42 -0300

    Update starting_container.sh

--- starting_container.sh
+++ starting_container.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+echo ************************
+echo paramters $1 $2 $3
+echo ************************
+echo ************************
 echo checkout branch $1
+echo ************************
 git checkout $1
+echo ************************
 echo pulling repository $1
+echo ************************
 git pull origin $1
+echo ************************
 echo cleaning and installing
+echo ************************
 mvn clean install
-echo starting app for profile $2 port $3
+echo ************************
+echo starting app to profile $2 port $3
+echo ************************
 mvn spring-boot:run -Dspring.profiles.active=$2 -Dserver.port=$3
